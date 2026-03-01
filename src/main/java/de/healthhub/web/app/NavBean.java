3b0da1ca7a74168b7fdf73e95c9f0c6874e371b2
eduard-roth <eduard.roth@data4treat.com>
2026-03-01 17:09:36 +0100

    Initial commit - Jakarta EE HealthHub base setup

---
+++ src/main/java/de/healthhub/web/app/NavBean.java
@@ -0,0 +1,17 @@
+package de.healthhub.web.app;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import java.io.IOException;
+
+@Named
+@RequestScoped
+public class NavBean {
+
+    public void redirectAdmin() throws IOException {
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        ec.redirect(ec.getRequestContextPath() + "/admin/dashboard.xhtml");
+    }
+}
