c23b5135d6036a55023df30fa10213dab0422a54
eduard-roth <eduard.roth@data4treat.com>
2026-03-28 14:35:37 +0100

    added keycloak support

--- src/main/java/de/healthhub/bootstrap/HealthHubApplication.java
+++ src/main/java/de/healthhub/bootstrap/HealthHubApplication.java
@@ -1,8 +1,25 @@
 package de.healthhub.bootstrap;
 
+import de.healthhub.auth.api.AuthResource;
+import de.healthhub.health.api.HealthResource;
+import de.healthhub.measurement.api.MeasurementResource;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ApplicationPath("/api")
 public class HealthHubApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+        classes.add(HealthResource.class);
+        classes.add(AuthResource.class);
+        classes.add(MeasurementResource.class);
+
+        return classes;
+    }
 }
